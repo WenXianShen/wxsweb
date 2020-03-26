153e91f6c8618f9778aa423ac080b5be8df408e9
ymweng <ymweng@ieslab-sh.cn>
2020-03-26 14:52:40 +0800

    修复请求失败不弹框提示的bug

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import router from './router'
 import 'view-design/dist/styles/iview.css'
 import './permission'
-import ElementUI, {Message} from 'element-ui'
+import ElementUI from 'element-ui'
 import Util from './assets/js/util.js'
 import Api from './assets/js/api.js'
 import Resource from 'vue-resource'
@@ -13,7 +13,7 @@ Vue.use(ViewUI)
 Vue.use(ElementUI)
 Vue.use(Resource)
 // 请求API
-Vue.prototype._API = Api
+ Vue.prototype._API = Api
 // 通用方法
 Vue.use(Util)
 // TODO 项目地址设定
@@ -27,7 +27,7 @@ Vue.http.interceptors.push(function (request, next) {
   // 请求出口处理
   next(function (response) {
     if (response.status !== 200) {
-      Message.error('访问失败，请重试！')
+      Vue.prototype.$Message.error('访问失败，请重试！')
     }
     // 处理网络异常情况统一或添加其它特殊标识
   })
